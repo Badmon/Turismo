8fe7b97247bbf2f777a6a29f4f61b77e4ba0e44c
Badmon <jleonh@autonoma.edu.pe>
2019-09-15 00:40:42 -0500

    commit

--- app/src/main/java/com/example/turismo/MainActivity.kt
+++ app/src/main/java/com/example/turismo/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.turismo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +18,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun initFunc(){
         //implement button action
+        btn_scan.setOnClickListener{
+            initScan()
+        }
+    }
+
+    private fun initScan(){
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data )
+
+        if(result != null){
+            if(result.contents == null){
+                Toast.makeText(this,"The data is empty", Toast.LENGTH_LONG).show()
+            }else{
+                et_value.setText(result.contents.toString())
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
